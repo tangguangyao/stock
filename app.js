6c3443f9370500dcf19c3e4f7f03931b31d04d81
唐老鸭光光 <gytang1986@gmail.com>
2013-11-12 22:53:17 +0800

    前端套ejs

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
   , MongoStore = require('connect-mongo')(express)
@@ -38,9 +37,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+routes(app)
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,25 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+module.exports = function(app){
+  app.get('/',function(req,res){
+    res.render('index', { title: 'Express' });
+  });
+
+  app.get('/login',function(req,res){
+    res.render('login', { title: 'Express' });
+  });
+
+  app.get('/people',function(req,res){
+    res.render('people', { title: 'Express' });
+  });
+
+  app.get('/setting',function(req,res){
+    res.render('setting', { title: 'Express' });
+  });
+
+  app.get('/stock',function(req,res){
+    res.render('stock', { title: 'Express' });
+  });
+
 };
