2e09e708f011b57a7c598d513892446f6f22aaa7
唐老鸭光光 <gytang1986@gmail.com>
2013-11-19 18:18:16 +0800

    少量修改

--- public/js/tgy/index.js
+++ public/js/tgy/index.js
@@ -4,7 +4,7 @@ function IndexCtrl($scope, $http, $templateCache) {
   //定时获取关注数据
   
   $scope.method = 'JSONP';
-  var stockCode='SH600267,SZ002194,SH600171';
+  var stockCode=$("#stockList").attr("my-stock");
   //$scope.url = 'http://xueqiu.com/stock/quote.json?code='+pathUrl+'&callback=JSON_CALLBACK';
   $scope.url = 'http://xueqiu.com/stock/quote.json?code='+stockCode+'&key=47bce5c74f&access_token=aZy51015GfDGsvsNtit2XY&_=1384782884165&callback=JSON_CALLBACK'
   $scope.code = null;
@@ -14,24 +14,21 @@ function IndexCtrl($scope, $http, $templateCache) {
     $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
       success(function(data, status) {
       	$scope.stocks=data.quotes;
-        // $scope.status = status;
-        // //数据处理
-        // userName=data.quotes[0].name;
-        // var time=new Date(data.quotes[0].time);
-        // data.quotes[0].time=time.getTime();
-
-        // if(data.quotes[0].current>data.quotes[0].last_close){
-        //   data.quotes[0].zdClass="red";
-        //   data.quotes[0].zdf="+";
-        // }else if(data.quotes[0].current=data.quotes[0].last_close){
-        //   data.quotes[0].zdClass="";
-        //   data.quotes[0].zdf="";
-        // }else{
-        //   data.quotes[0].zdClass="green";
-        //   data.quotes[0].zdf="-";
-        // }
-
-        // $scope.stock = data.quotes[0];
+        for(var i=0,l=data.quotes.length;i<l;i++){
+          data.quotes[i].volume=(data.quotes[i].volume/10000).toFixed(2);
+          data.quotes[i].marketCapital=(data.quotes[i].marketCapital/100000000).toFixed(2)
+          if(Number(data.quotes[i].change)>0){
+            data.quotes[i].zdClass="red";
+            data.quotes[i].change="+"+data.quotes[i].change;
+            data.quotes[i].percentage="+"+data.quotes[i].percentage+"%";
+          }else if(Number(data.quotes[i].change)==0){
+            data.quotes[i].zdClass="";
+            data.quotes[i].percentage=data.quotes[i].percentage+"%";
+          }else{
+            data.quotes[i].zdClass="green";
+            data.quotes[i].percentage=data.quotes[i].percentage+"%";
+          }
+        }
       });
   }
   ajaxStock();
@@ -45,5 +42,35 @@ function IndexCtrl($scope, $http, $templateCache) {
         setTimeout(start,10000);
      }
   }
-  //start();
+  start();
+
+
+  //绑定事件
+  $scope.delStock = function(stock) {
+    var stockUid=stock.symbol.toLocaleLowerCase();
+    var watchUrl="watchStock?uid="+stockUid+"&name="+stock.name+"&beWatchName="+$("#headShowName").text()+"&beWatchTop="+$("#headShowName").attr("top")+"&add=0";
+    $http({method: "GET", url: watchUrl, cache: $templateCache}).
+        success(function(data, status) {
+          $scope.status = status;
+          if(data.ok){
+
+          }
+        });
+    // if($("#headShowName").attr("login")=="in"){
+    //   watchUrl="watchStock?uid="+pathUrl+"&name="+userName+"&beWatchName="+$("#headShowName").text()+"&beWatchTop="+$("#headShowName").attr("top")+"&add=1";
+    //   $http({method: "GET", url: watchUrl, cache: $templateCache}).
+    //     success(function(data, status) {
+    //       $scope.status = status;
+    //       if(data.ok){
+    //         //$scope.isWatch=true;//显示取消
+    //         $("#butIswatch").hide();
+    //         $("#butNowatch").show();
+    //       }
+    //     });
+    // }else{
+    //   alert("请先登录")
+    // }
+  }
+
+
 }
